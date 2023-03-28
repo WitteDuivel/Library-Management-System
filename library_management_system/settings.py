c00fff653ee5ef599874a94d6a665111714a7011
Vivek Chakravorty <mrzero@duck.com>
2023-03-28 12:09:23 +0530

    Resolved an issue with security

--- library_management_system/settings.py
+++ library_management_system/settings.py
@@ -19,13 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t@_aldd_s6*#ylt(iq_chve88q1n^%sy6c*shb638&ut6m(1ry"
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.29.110']
+ALLOWED_HOSTS = []
 
 
 # Application definition
